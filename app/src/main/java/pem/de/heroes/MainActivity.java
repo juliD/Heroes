bd965085d88b1632d347e15ba7929ddcb868d53c
Julia Doerner <doernerj@cip.ifi.lmu.de>
2017-06-12 22:03:34 +0200

    Anonyme Authentisierung hinzugefuegt

--- app/src/main/java/pem/de/heroes/MainActivity.java
+++ app/src/main/java/pem/de/heroes/MainActivity.java
@@ -1,32 +1,46 @@
 package pem.de.heroes;
 
 import android.content.Intent;
-import android.graphics.drawable.GradientDrawable;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
 
+    private static final String TAG = "MainActivity";
     private ViewPager viewPager;
     private static final String ARG_TYPE = "activity_type";
     FloatingActionButton fab;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Check if user is already logged in
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+
+        } else {
+            signInAnonymoulsy();
+
+        }
 
         viewPager = (ViewPager) findViewById(R.id.pager);
         TabsPagerAdapter tabsAdapter = new TabsPagerAdapter(getSupportFragmentManager());
@@ -61,6 +75,40 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         TabLayout.Tab tab = tabLayout.getTabAt(1);
         tab.select();
 
+    }
+    public void signInAnonymoulsy(){
+        auth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInAnonymously:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            update(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInAnonymously:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            update(null);
+                        }
+
+                    }
+                });
+    }
+
+    public void update(FirebaseUser currentUser){
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        update(currentUser);
     }
 
 
