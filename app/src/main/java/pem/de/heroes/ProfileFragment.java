f285d8f83917c66fc3568fa282666f8d49c312e8
Johannes Ismair <ismair.johannes@gmail.com>
2017-06-20 21:55:19 +0200

    profile fragment gets refreshed when settings change

--- app/src/main/java/pem/de/heroes/ProfileFragment.java
+++ app/src/main/java/pem/de/heroes/ProfileFragment.java
@@ -5,41 +5,45 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private SharedPreferences sharedPreferences;
+    private TextView usernameView;
 
     public ProfileFragment() {
 
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        SharedPreferences sharedPref = getActivity().getSharedPreferences("pem.de.hero.userid", Context.MODE_PRIVATE);
+        sharedPreferences = getActivity().getSharedPreferences("pem.de.hero.userid", Context.MODE_PRIVATE);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
 
-
-        //write profilename into textview from shared preferences
-        TextView profil_name = (TextView) view.findViewById(R.id.profile_name);
-        profil_name.setText(sharedPref.getString("username","Anonym"));
-
-
+        usernameView = (TextView) view.findViewById(R.id.profile_name);
+        usernameView.setText(sharedPreferences.getString("username", ""));
 
         return view;
     }
 
-
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Log.e("Profile", key);
+        if (key.equals("username")) {
+            usernameView.setText(sharedPreferences.getString("username", ""));
+        }
+    }
 }
